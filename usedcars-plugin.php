6265fb80f094587ef2ac0cd89055e2838fe437c1
14Deveshjoshi <31435747+14Deveshjoshi@users.noreply.github.com>
2019-05-30 13:29:11 +0530

    Add files via upload

---
+++ usedcars-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+*@package Usedcarsplugin
+*/
+
+/*
+
+* Plugin Name : Usedcars Plugin
+* PLugin URI : https://github.com/14Deveshjoshi/plugin/
+* Description: This plugin adds a form for get data from database.
+ * Version:     1.0
+ * Author:      Devesh Joshi
+ * Author URI:  http://www.deveshjoshi.info
+*/
